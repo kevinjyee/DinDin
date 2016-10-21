a6bd741761812d256711f28a1ae22a0d015aeb73
RISEN_MAMBA\Davin <dsiu@utexas.edu>
2016-10-20 22:23:37 -0500

    added Preference class

---
+++ app/src/main/java/com/example/dindin/Preferences.java
@@ -0,0 +1,15 @@
+package com.example.dindin;
+
+import java.util.List;
+
+/**
+ * Created by Davin on 10/20/2016.
+ */
+
+public class Preferences {
+    String preferredTask;
+    float matchDistance; //radius size (miles) within user location for matching
+    int minAge;
+    int maxAge;
+    List<String> preferredCuisines;
+}
