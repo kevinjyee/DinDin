82adc4c91880a089450799c3deb541c668fb0efd
RISEN_MAMBA\Davin <dsiu@utexas.edu>
2016-10-20 22:08:30 -0500

    Created User class

---
+++ app/src/main/java/com/example/dindin/User.java
@@ -0,0 +1,28 @@
+package com.example.dindin;
+
+import com.facebook.Profile;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Davin on 10/20/2016.
+ */
+
+public class User {
+    String name;
+    Date birthday;
+    String gender;
+    //location (Google maps)
+    Profile facebookUser;
+    int userRating;
+    List<User> matches;
+    //List<Recipe> starredRecipes;
+    String preferredTask;
+
+    public User(){}
+
+    public User(Profile profile){
+        this.facebookUser = profile;
+    }
+}
