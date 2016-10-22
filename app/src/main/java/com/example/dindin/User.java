28987710d480bc55db67f6c290a3702f0b0af159
kevinjyee <kevjyee@gmail.com>
2016-10-21 19:09:22 -0500

    Added splash page

--- app/src/main/java/com/example/dindin/User.java
+++ app/src/main/java/com/example/dindin/User.java
@@ -1,29 +1,42 @@
 package com.example.dindin;
 
+
 import com.facebook.Profile;
 
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created by Davin on 10/20/2016.
- */
-
 public class User {
-    String name;
-    Date birthday;
-    String gender;
-    //location (Google maps)
-    Profile facebookUser;
-    int userRating;
-    List<User> matches;
-    //List<Recipe> starredRecipes;
-    String preferredTask;
+    private int id;
+    private String name;
+    private String address;
+    public User()
+    {
+    }
+    public User(int id,String name,String address)
+    {
+        this.id=id;
+        this.name=name;
+        this.address=address;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public User(){}
-
-    public User(Profile profile){
-        this.facebookUser = profile;
-        this.name = profile.getName();
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getName() {
+        return name;
     }
 }
+
